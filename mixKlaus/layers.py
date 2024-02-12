Fix weight update (avoid overflow)

mixKlaus/layers.py
@@ -708,6 +708,7 @@ class NNMFMixerAttentionHeadsDynamicWeights(
     def _update_weight(self, h, reconstruction, input):
         nnmf_update = input / reconstruction
         self.global_weight.data *= torch.einsum("bohf,bihf->oi", h, nnmf_update)
+        self.global_weight = self._secure_tensor(self.global_weight)
         self.global_weight = F.normalize(self.global_weight, p=1, dim=1)
 
 
@@ -740,7 +741,8 @@ class NNMFMixerAttentionHeadsConvDynamicWeights(
         normalize_h: bool = True,
         normalize_h_dim: int | None = -1,
     ):
-        super().__init__(
+        NNMFMixerAttentionHeadsConv.__init__(
+            self,
             kernel_size=kernel_size,
             embed_dim=embed_dim,
             stride=stride,
@@ -807,10 +809,19 @@ class NNMFMixerAttentionHeadsConvDynamicWeights(
             3, 0, 1, 2
         )  # B, T, H, D -> HD, B, P, P
 
-        self.global_weight_conv.data *= torch.conv2d(
-            h, nnmf_update, stride=self.stride, padding=self.padding
-        ) 
-        # TODO: fix weights in each head
+        # Devide by the batch size to avoid overflow
+        nnmf_update /= nnmf_update.shape[1]
+        h /= h.shape[1]
+
+        new_weight = F.conv2d(
+            h, nnmf_update, stride=self.stride, padding=self.padding, 
+        )
+
+        # TODO: update w rate TODO: fix weights in each head
+        self.global_weight_conv.data *= new_weight
+
+        self.global_weight_conv = self._secure_tensor(self.global_weight_conv)
+
         self.global_weight_conv = F.normalize(
             self.global_weight_conv, p=1, dim=(1, 2, 3)
         )

nnmf/modules.py
@@ -40,6 +40,10 @@ class NNMFLayer(nn.Module):
             "solver",
             "all_grads",
         ], f"backward_method must be one of 'fixed_point', 'solver', 'all_grads', got {backward_method}"
+        if not activate_secure_tensors:
+            print(
+                "[WARNING] 'activate_secure_tensors' is False! This may lead to numerical instability."
+            )
 
         self.n_iterations = n_iterations
         self.activate_secure_tensors = activate_secure_tensors
@@ -104,12 +108,13 @@ class NNMFLayer(nn.Module):
 
     def _nnmf_iteration(self, input):
         reconstruction = self._reconstruct(self.h)
+        reconstruction = self._secure_tensor(reconstruction)
         if self.normalize_reconstruction:
             reconstruction = F.normalize(
                 reconstruction, p=1, dim=self.normalize_reconstruction_dim, eps=1e-20
             )
-        reconstruction = self._secure_tensor(reconstruction)
         nnmf_update = input / reconstruction
+        assert nnmf_update.isfinite().all() and nnmf_update.isnan().any()==0, (nnmf_update, input, reconstruction, input.isfinite().all(), reconstruction.isfinite().all(), input.isnan().any(), reconstruction.isnan().any(), reconstruction.min(), reconstruction.max())
         new_h = self.h * self._forward(nnmf_update)
         if self.h_update_rate == 1:
             h = new_h